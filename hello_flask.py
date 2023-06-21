e46bddfabc02e92164df7abd651d9a5b813f23ca
praveenpy273 <praveenpaliwal273@gmail.com>
2023-06-21 20:43:49 +0100

    restored to last modified script

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello from Flask'
+
+@app.route('/search4')
+def do_search():
+    return str(search4letters('life, the universe, and everything', 'eiru!'))
+
 
 app.run()
