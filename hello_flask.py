e85d44a1caca6c3bf21486286b4758d9dcb1ceee
praveenpy273 <praveenpaliwal273@gmail.com>
2023-06-23 19:43:01 +0100

    added new function to the code

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from vsearch
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello from Flask'
+
+@app.route('/search4')
+def search4letters(phrase ='life,the universe,and everything', letters = 'eiru!') -> set:
+    """Return the letters which are common with phrase"""
+    return str(set(letters).intersection(set(phrase)))
 
 app.run()
