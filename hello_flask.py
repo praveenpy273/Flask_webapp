af5e8b3bc80bd89826cc855590550fe04a6845d2
praveenpy273 <praveenpaliwal273@gmail.com>
2023-07-03 19:23:42 +0100

    pending commits

--- hello_flask.py
+++ hello_flask.py
@@ -7,8 +7,8 @@ def hello():
     return 'Hello from Flask'
 
 
-@app.route('/search4/')
-def search4letters(phrase ='life,the universe,and everything', letters = 'eiru!') -> set:
+@app.route('/search4',methods=['POST'])
+def search4letters(phrase ='life,the universe,and everything', letters = 'eiru,!') -> set:
     """Return the letters which are common with phrase"""
     return str(set(letters).intersection(set(phrase)))
 
@@ -17,4 +17,4 @@ def entry_page():
     return render_template('entry.html', the_title = 'Welcome to search4letters on the web!')
 
 
-app.run()
+app.run(debug = True)
